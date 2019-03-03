bc1c70c9ff0cf339e558794f720971cd3a8ed7ed
YannRicci <yann.ricci@telecomnancy.net>
2019-03-03 18:38:46 +0100

    New address

--- app/src/main/java/com/example/tp1/FetchTaskMail.java
+++ app/src/main/java/com/example/tp1/FetchTaskMail.java
@@ -11,7 +11,7 @@ public class FetchTaskMail extends AsyncTask<String, Void, Void> {
 
     @Override
     protected Void doInBackground(String... strings) {
-        GMailSender sender = new GMailSender("yann.ricci@gmail.com", "*****");
+        GMailSender sender = new GMailSender("JavaMail.TP@gmail.com", "V3ry_strong_pw");
         try {
             sender.sendMail("Test", "body", "yann.ricci@telecomnancy.net", "yann.ricci@gmail.com");
             Log.d("MainActivity","envoie?");
